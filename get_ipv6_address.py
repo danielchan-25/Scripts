45220867297c7e4d7497aac67072d83f10e1163d
danielchan-25 <alexchan0325@gmail.com>
2024-07-21 10:53:18 +0800

    删除多余代码

--- get_ipv6_address.py
+++ get_ipv6_address.py
@@ -1,12 +1,6 @@
-import json
 import time
-import hmac
-import base64
-import hashlib
-import requests
 import subprocess
-import urllib.parse
-
+from dingbot import dingbot_notice
 
 # 日期：2024年7月10日
 # 功能：通过跳板机，获取指定服务器的IPV6地址，并通过钉钉告警提醒。
@@ -16,32 +10,6 @@ import urllib.parse
 host = '192.168.2.12'
 username = 'admin'
 password = 'NPw4ks5SP4pdrxdzK9Gc'
-api_url = 'https://oapi.dingtalk.com/robot/send?access_token='  # 钉钉机器人api地址
-api_secret = '' # 钉钉机器人 Webhook
-
-
-def dingbot_notice(content):
-    ''' 钉钉告警 '''
-    def get_timestamp_sign():
-        timestamp = str(round(time.time() * 1000))
-        secret = api_secret
-        secret_enc = secret.encode('utf-8')
-        string_to_sign = '{}\n{}'.format(timestamp, secret)
-        string_to_sign_enc = string_to_sign.encode('utf-8')
-        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
-        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-        return timestamp, sign
-
-    def get_signed_url():
-        timestamp, sign = get_timestamp_sign()
-        webhook = api_url + "&timestamp=" + timestamp + "&sign=" + sign
-        return webhook
-
-    webhook = get_signed_url()
-    header = {"Content-Type": "application/json", "Charset": "UTF-8"}
-    message = {"msgtype": "text", "text": {"content": content}}
-    message_json = json.dumps(message)
-    requests.post(url=webhook, data=message_json, headers=header).json()
 
 
 def get_ipaddress():
